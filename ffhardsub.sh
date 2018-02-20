bd6ba6ae13e8e5a0be1b632b58c11981d48b6bfe
Behrooz-Ramezani <b3hrooz.ram3zani@gmail.com>
2018-02-20 04:15:36 +0330

    fix bug hardsub.sh

--- ffhardsub.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-vidoe="$1"
-subtitle="$2"
-logo="$3"
-name=$(echo "$1" | rev | cut -f2 -d '.' | rev)
-ffpb -i "$1" -i "$3" -filter_complex "[0:v][1:v]overlay=W-w-5:5,ass="$2"[out]"  -map "[out]" -map 0:a $name-watermark.mkv
